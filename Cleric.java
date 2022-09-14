7dba96359a25a129ea5e22afad2b2d98029881c6
NakagawaYuuto <github.nakagawa@gmail.com>
2022-09-14 20:25:12 +0900

    コメントの追加

--- Cleric.java
+++ Cleric.java
@@ -5,7 +5,7 @@ public class Cleric {
     int hp = 50;
     final int maxHp = 50;
 
-    int mp = 10;
+    int mp = 5;
     final int maxMp = 10;
 
     public void selfAid(){
@@ -13,15 +13,19 @@ public class Cleric {
         this.mp -= 5;
         this.hp = this.maxHp;
         System.out.println("HPが全回復した。");
+        System.out.println("今現在のMPは" + this.mp);
     }
 
     public int pray(int sec){
         System.out.println("聖職者は祈った。");
-        int recover = new Random().nextInt(3) + sec;
-        int recoverActual = Math.min(this.maxHp - this.mp, recover);
+        Random rand = new Random();
+        int recover = rand.nextInt(3) + sec;
+        //  Random(),nextInt():nextInt() は 0 から引数に指定した値未満の整数を返します。
+        int recoverActual = Math.min(this.maxMp - this.mp, recover);
         //  Math.min:静的関数で、引数で渡されたもののうち最小の値を返します
         this.mp += recoverActual;
         System.out.println("MPが" + recoverActual + "回復した。");
+        System.out.println("今現在のMPは" + this.mp);
         return recoverActual;
     }
 }
